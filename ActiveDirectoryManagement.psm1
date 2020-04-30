23278daeb4d1cd98b71faab1377dffd7fbbbb227
Jeffrey Patton <jeffrey@patton-tech.com>
2020-04-30 13:57:23 -0500

    forgot to remove in-line help

--- ActiveDirectoryManagement.psm1
+++ ActiveDirectoryManagement.psm1
@@ -323,47 +323,6 @@ Function Get-DomainName {
     }
 }
 Function Get-UserGroupMembership {
-    <#
-        .SYNOPSIS
-            Get a list of groups as displayed on the user objects Member of tab
-        .DESCRIPTION
-            This function returns a listing of groups that the user is a direct
-            member of. This is the same list that should appear in the Member Of
-            tab in Active Directory Users and Computers.
-        .PARAMETER UserDN
-            The DistinguishedName of the user object
-        .EXAMPLE
-            Get-UserGroupMembership -UserDN "CN=useraccount,OU=employees,DC=company,DC=com"
-
-            GroupDN
-            -------
-            CN=AdminStaff,OU=Groups,DC=company,DC=com
-            CN=ServerAdmin,OU=Groups,DC=Company,DC=com
-
-            Description
-            -----------
-            This shows the basic syntax of a user in the local domain.
-        .EXAMPLE
-            Get-UserGroupMembership -UserDN "CN=S-1-5-17,CN=ForeignSecurityPrincipals,DC=company,DC=com"
-
-            GroupDN
-            -------
-            CN=IIS_IUSRS,CN=Builtin,DC=company,DC=com
-
-            Description
-            -----------
-            This function also works against FSP's in your domain.
-        .NOTES
-            FunctionName : Get-UserGroupMembership
-            Created by   : Jeff Patton
-            Date Coded   : 09/22/2011 12:53:23
-
-            This script runs in the context of the user and as such the user
-            will need to have the requisite permissions to view the group membership
-            of a given user object.
-        .LINK
-            https://code.google.com/p/mod-posh/wiki/ActiveDirectoryManagement#Get-UserGroupMembership
-    #>
     [CmdletBinding(HelpUri = 'https://github.com/jeffpatton1971/ActiveDirectoryManagement/blob/1.1.0/docs/Get-UserGroupMembership.md#get-usergroupmembership')]
     Param
     (
